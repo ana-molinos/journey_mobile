218e7b3661e5543f01d04ad45706ed7d536f39ba
ana-molinos <anamolinos52@gmail.com>
2026-09-10 17:41:28 -0300

    Initial commit

---
+++ Journey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Journey
+//
+//  Created by Ana Julia Molinos Leite Da Silva on 10/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
