db9fe745868f80740a660edfd0001c094559498b
ana-molinos <anamolinos52@gmail.com>
2026-09-15 16:56:26 -0300

    feat: login screen UI finished

--- Journey/Scenes/Login/LoginView.swift
+++ Journey/Scenes/Login/LoginView.swift
@@ -9,6 +9,17 @@ import Foundation
 import UIKit
 
 class LoginView: UIView {
+    
+    let logoImage: UIImageView = {
+        let image = UIImageView()
+        
+        image.image = UIImage(named: "logo")
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        return image
+    }()
+    
     let emailTextField: LoginTextField = {
         let textField = LoginTextField()
         
@@ -27,6 +38,31 @@ class LoginView: UIView {
         return textField
     }()
     
+    let passwordRecoveryButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("Esqueci a senha", for: .normal)
+        button.setTitleColor(Colors.primaryBlue, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14, weight: .bold)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        /// **TO DO:** add button action
+        
+        return button
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("Fazer login", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
+        button.backgroundColor = Colors.primaryPurple
+        button.layer.cornerRadius = 8
+        button.translatesAutoresizingMaskIntoConstraints = false
+        /// **TO DO:** add button action
+        
+        return button
+    }()
+    
     init() {
         super.init(frame: .zero)
         
@@ -40,20 +76,36 @@ class LoginView: UIView {
     private func setup() {
         self.addSubview(emailTextField)
         self.addSubview(passwordTextField)
+        self.addSubview(logoImage)
+        self.addSubview(passwordRecoveryButton)
+        self.addSubview(loginButton)
         
         setupConstraints()
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            emailTextField.topAnchor.constraint(equalTo: self.topAnchor, constant: 100),
+            
+            logoImage.topAnchor.constraint(equalTo: self.topAnchor, constant: 128),
+            logoImage.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            logoImage.widthAnchor.constraint(equalToConstant: 152),
+            
+            emailTextField.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 64),
             emailTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 24),
             emailTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -24),
-//            emailTextField.bottomAnchor.constraint(equalTo: self.bottomAnchor),
             
             passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 16),
             passwordTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 24),
             passwordTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -24),
+            
+            passwordRecoveryButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 15),
+            passwordRecoveryButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -24),
+            
+            loginButton.topAnchor.constraint(equalTo: passwordRecoveryButton.bottomAnchor, constant: 47),
+            loginButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            loginButton.heightAnchor.constraint(equalToConstant: 48),
+            loginButton.widthAnchor.constraint(equalToConstant: 256)
+            
         ])
     }
 }
