0da69d29fb3253f69e08ae5608594e0d04445412
ana-molinos <anamolinos52@gmail.com>
2026-09-15 16:09:49 -0300

    chore: add design system colors

--- Journey/DesignSystem/Colors.swift
+++ Journey/DesignSystem/Colors.swift
@@ -9,9 +9,9 @@ import Foundation
 import UIKit
 
 public struct Colors {
-    static let primaryPurple = UIColor(red: 163, green: 74, blue: 135, alpha: 1)
-    static let secondaryPurple = UIColor(red: 72, green: 87, blue: 148, alpha: 1)
-    static let primaryBlue = UIColor(red: 44, green: 82, blue: 130, alpha: 1)
-    static let secondaryBlue = UIColor(red: 180, green: 220, blue: 250, alpha: 1)
-    static let gray = UIColor(red: 230, green: 230, blue: 238, alpha: 1)
+    static let primaryPurple = UIColor(red: 163/255, green: 74/255, blue: 135/255, alpha: 1)
+    static let secondaryPurple = UIColor(red: 72/255, green: 87/255, blue: 148/255, alpha: 1)
+    static let primaryBlue = UIColor(red: 44/255, green: 82/255, blue: 130/255, alpha: 1)
+    static let secondaryBlue = UIColor(red: 180/255, green: 220/255, blue: 250/255, alpha: 1)
+    static let gray = UIColor(red: 230/255, green: 230/255, blue: 238/255, alpha: 1)
 }
